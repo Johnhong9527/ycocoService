f466ecea703726f11ca4f93ed3f56d1a8d3acc34
seam <honghaitzz11@gmail.com>
2017-12-14 12:15:53 +0800

    跨域设置

--- app.js
+++ app.js
@@ -36,22 +36,10 @@ app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
   // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
 
-//
-app.all('*',function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Content-Type,Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
-  res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE,OPTIONS');
-  if (req.method == 'OPTIONS') {
-    res.send(200);
-  }
-  else {
-    next();
-  }
-});
+
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,21 @@ var router = express.Router();
 router.get('/', function (req, res, next) {
   res.render('index', {title: 'Express'});
 });
+
+//
+router.all('*',function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type,Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
+  res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE,OPTIONS');
+  if (req.method == 'OPTIONS') {
+    res.send(200);
+  }
+  else {
+    next();
+  }
+});
+
+
 router.get('/form', function (req, res) {
   var data = [
     {name: '123', num: 12},
@@ -11,19 +26,19 @@ router.get('/form', function (req, res) {
     {name: '123', num: 12},
     {name: '123', num: 12}
   ];
-//   res.set({
-//   'Content-Type': 'application/x-www-form-urlencoded',
-//   'Access-Control-Allow-Origin': 'http://ycoco.xyz',
-// });
+  //   res.set({
+  //   'Content-Type': 'application/x-www-form-urlencoded',
+  //   'Access-Control-Allow-Origin': 'http://ycoco.xyz',
+  // });
   // console.log(JSON.stringify(req.query))
   res.json(data);
   return;
 });
 router.post('/date', function (req, res) {
-//   res.set({
-//   'Content-Type': 'application/x-www-form-urlencoded',
-//   'Access-Control-Allow-Origin': 'http://ycoco.xyz',
-// });
+  res.set({
+    'Content-Type': 'application/x-www-form-urlencoded',
+    'Access-Control-Allow-Origin': 'http://ycoco.xyz',
+  });
   res.jsonp('ok');
 })
 
