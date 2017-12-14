0571c0c3eee44ef92a2dee079fdd0aceb01c7bde
seam <honghaitzz11@gmail.com>
2017-12-14 17:30:57 +0800

    .

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,16 @@ var express = require('express');
 var router = express.Router();
 // mongodb
 var MongoClient = require('mongodb').MongoClient;
-
+var DB_BOOKS = 'mongodb://127.0.0.1:27017/ycoco'; // 数据库
+MongoClient.connect(DB_BOOKS, function(err, db) {
+  console.log("连接成功！");
+  db.collection('ycoco').find().toArray(function(err, result) {
+    if (err) {
+      throw err;
+    }
+    console.log(result);
+  });
+});
 // 全局变量
 var data = [
   {name: '123', num: 12},
@@ -15,15 +24,12 @@ router.get('/', function (req, res, next) {
   res.render('index', {title: 'Express'});
 });
 router.get('/form', function (req, res) {
-  MongoClient.connect('mongodb://127.0.0.1:27017/ycoco', function(err, db) {
-    if (err) {
-      throw err;
-    }
-    db.collection('ycoco').find().toArray(function(err, result) {
+  MongoClient.connect(DB_BOOKS, function(err, db) {
+    console.log("连接成功！");
+    db.collection({}).find().toArray(function(err, result) {
       if (err) {
         throw err;
       }
-      // console.log(result);
       res.send(result);
     });
   });
