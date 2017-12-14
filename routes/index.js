d616d0793f3516b571f7a44758e9c4f2eaa4df47
seam <honghaitzz11@gmail.com>
2017-12-14 11:28:56 +0800

    跨域设置

--- routes/index.js
+++ routes/index.js
@@ -13,7 +13,7 @@ router.get('/form', function (req, res) {
   ];
   res.set({
   'Content-Type': 'application/x-www-form-urlencoded',
-  'Access-Control-Allow-Origin': 'http://localhost:8080',
+  'Access-Control-Allow-Origin': 'http://ycoco.xyz',
 });
   // console.log(JSON.stringify(req.query))
   res.json(data);
