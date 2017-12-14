c79da24574ac752ef19e6f55ccad956b54afc36c
seam <honghaitzz11@gmail.com>
2017-12-14 12:17:10 +0800

    跨域设置

--- app.js
+++ app.js
@@ -22,6 +22,19 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// 跨域
+app.all('*',function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type,Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
+  res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE,OPTIONS');
+  if (req.method == 'OPTIONS') {
+    res.send(200);
+  }
+  else {
+    next();
+  }
+});
+
 app.use('/', index);
 app.use('/users', users);
 // catch 404 and forward to error handler
--- routes/index.js
+++ routes/index.js
@@ -5,20 +5,7 @@ router.get('/', function (req, res, next) {
   res.render('index', {title: 'Express'});
 });
 
-//
-router.all('*',function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Content-Type,Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
-  res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE,OPTIONS');
-  if (req.method == 'OPTIONS') {
-    res.send(200);
-  }
-  else {
-    next();
-  }
-});
-
-
+// 跨域
 router.get('/form', function (req, res) {
   var data = [
     {name: '123', num: 12},
