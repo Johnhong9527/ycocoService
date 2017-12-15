1ccb3920e3d2243c832f89e492e2c9faa8f9e317
seam <honghaitzz11@gmail.com>
2017-12-15 16:27:29 +0800

    .

--- app.js
+++ app.js
@@ -4,12 +4,14 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var helmet = require('helmet'); // Deal with security risks
+var ecstatic = require('../lib/ecstatic'); 
 var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
-
+// Deal with security risks
+app.use(helmet());
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
