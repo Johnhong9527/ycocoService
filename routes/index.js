0b8248599d52969388c28588c5de04c3c028c684
seam <honghaitzz11@gmail.com>
2017-12-15 14:19:22 +0800

    add admin UI

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,5 @@
 var express = require('express');
 var router = express.Router();
-// mongodb  "mongodb": "2.2.33",
-// var MongoClient = require('mongodb').MongoClient;
-// var DB_BOOKS = 'mongodb://127.0.0.1:27017/ycoco'; // 数据库
 // getting-started.js
 var MongoClient = require('mongodb').MongoClient,
 assert = require('assert');
@@ -10,30 +7,12 @@ assert = require('assert');
 var url = 'mongodb://127.0.0.1:27017/ycoco';
 // Database Name
 var dbName = 'ycoco';
-// 全局变量
-var data = [
-  {name: '123', num: 12},
-  {name: '123', num: 12},
-  {name: '123', num: 12},
-  {name: '123', num: 12}
-];
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.render('index', {title: 'Express'});
 });
 router.get('/form', function (req, res) {
   var thatRes = null;
-  // MongoClient.connect(DB_BOOKS, function(err, db) {
-  //   console.log("连接成功！");
-  //   db.collection('ycoco').find().toArray(function(err, result) {
-  //     if (err) {
-  //       throw err;
-  //     }
-  //     console.log("成功赋值！");
-  //     thatRes = result;
-  //   });
-  // });
-
   // Use connect method to connect to the server
   MongoClient.connect(url, function(err, client) {
     assert.equal(null, err);
@@ -43,14 +22,13 @@ router.get('/form', function (req, res) {
       if(err) {
         throw err;
       }
-      console.log("成功赋值！");
+      console.log("成功取值！");
       thatRes = result;
     })
   });
-
   setTimeout(function () {
     res.send(thatRes);
-  }, 300);
+  }, 200);
 });
 router.post('/date', function (req, res) {
   res.set({
