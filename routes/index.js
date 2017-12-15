119cc67fc32a2a596988453839845d0437b1570d
seam <honghaitzz11@gmail.com>
2017-12-15 10:37:35 +0800

    .

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
 // mongodb
-var MongoClient = require('mongodb').MongoClient;
-var DB_BOOKS = 'mongodb://127.0.0.1:27017/ycoco'; // 数据库
-
+// var MongoClient = require('mongodb').MongoClient;
+// var DB_BOOKS = 'mongodb://127.0.0.1:27017/ycoco'; // 数据库
+// getting-started.js
+var MongoClient = require('mongodb').MongoClient,
+assert = require('assert');
+// Connection URL
+const url = 'mongodb://127.0.0.1:27017/ycoco';
+// Database Name
+const dbName = 'ycoco';
 // 全局变量
 var data = [
   {name: '123', num: 12},
@@ -17,18 +23,42 @@ router.get('/', function (req, res, next) {
 });
 router.get('/form', function (req, res) {
   var thatRes = null;
-  MongoClient.connect(DB_BOOKS, function(err, db) {
-    console.log("连接成功！");
-    db.collection('ycoco').find().toArray(function(err, result) {
-      if (err) {
-        throw err;
-      }
-      console.log("成功赋值！");
-      thatRes = result;
+  // MongoClient.connect(DB_BOOKS, function(err, db) {
+  //   console.log("连接成功！");
+  //   db.collection('ycoco').find().toArray(function(err, result) {
+  //     if (err) {
+  //       throw err;
+  //     }
+  //     console.log("成功赋值！");
+  //     thatRes = result;
+  //   });
+  // });
+
+  var findDocuments = function(db, callback) {
+    // Get the documents collection
+    var collection = db.collection('documents');
+    // Find some documents
+    collection.find({}).toArray(function(err, docs) {
+      assert.equal(err, null);
+      console.log("Found the following records");
+      console.log(docs)
+      callback(docs);
+    });
+  }
+  // Use connect method to connect to the server
+  MongoClient.connect(url, function(err, client) {
+    assert.equal(null, err);
+    console.log("Connected successfully to server");
+    var db = client.db(dbName);
+    insertDocuments(db, function() {
+      findDocuments(db, function() {
+        client.close();
+      });
     });
   });
+
   setTimeout(function () {
-    res.send(thatRes);
+    res.send('ok');
   }, 300);
 });
 router.post('/date', function (req, res) {
