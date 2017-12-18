a15c32a19c2b85ed48123fc6df0dc30be85919ee
seam <honghaitzz11@gmail.com>
2017-12-18 12:25:31 +0800

    ajax调试

--- routes/users.js
+++ routes/users.js
@@ -2,9 +2,14 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.post('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   console.log(req.body);
   res.send('respond with a resource');
+});
+router.all('/admin', function (req, res, next) {
+  console.log(req.query);
+  console.log(req.body);
+res.send('ok');
 });
 
 module.exports = router;
