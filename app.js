87b96267b0c9843619dd753d6dbef4a5c9c29d07
seam <honghaitzz11@gmail.com>
2017-12-18 15:04:38 +0800

    重新创建项目

--- app.js
+++ app.js
@@ -4,22 +4,16 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var helmet = require('helmet'); // Deal with security risks
-var ecstatic = require('ecstatic');
+
 var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
-// Deal with security risks
-app.use(helmet());
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-//
-app.use(ecstatic({
-  root: `${__dirname}/public`,
-  showdir: true,
-}));
+
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
--- routes/users.js
+++ routes/users.js
@@ -2,14 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function (req, res, next) {
-  console.log(req.body);
+router.get('/', function(req, res, next) {
   res.send('respond with a resource');
-});
-router.all('/admin', function (req, res, next) {
-  console.log(req.query);
-  console.log(req.body);
-res.send('ok');
 });
 
 module.exports = router;
