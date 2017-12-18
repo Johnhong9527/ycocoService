998c4e4ccdedf9dc8efc56c14d3dbe54dfa124cb
seam <honghaitzz11@gmail.com>
2017-12-18 12:20:45 +0800

    ajax调试

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.post('/', function(req, res, next) {
-  console.log(req);
+  console.log(req.body);
   res.send('respond with a resource');
 });
 
